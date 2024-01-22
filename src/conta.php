29afc510621a1c2f063d1fe1ab432322140f00e5
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2024-01-22 13:31:15 +0000

    Criando metodo sacar(), com referencia this.

--- src/conta.php
+++ src/conta.php
@@ -4,4 +4,12 @@ class Conta {
     public string $cpfTitular;
     public string $nomeTitular;
     public float $saldo;
+
+    public function sacar(float $valorASacar){
+        if ($valorASacar > $this) {
+            echo "Saldo indisponível";
+        } else {
+            $this->saldo -= $valorASacar;
+        }
+    }
 }
