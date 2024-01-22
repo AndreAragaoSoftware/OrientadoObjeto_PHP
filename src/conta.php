c293d66829b8ad1e0e8ab906fcded7f0913732fb
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2024-01-22 13:52:11 +0000

    Criando o metodo transferir e retirando o if dos metodos.

--- src/conta.php
+++ src/conta.php
@@ -8,16 +8,31 @@ class Conta {
     public function sacar(float $valorASacar){
         if ($valorASacar > $this) {
             echo "Saldo indisponível";
-        } else {
-            $this->saldo -= $valorASacar;
-        }
+            return;
+        } 
+
+
+        $this->saldo -= $valorASacar;
     }
 
     public function depositar(float $valorADepositar): void{
         if ($valorADepositar < 0){
             echo "Valor precisa ser positivo";
-        } else {
-            $this->saldo += $valorADepositar;
-        }
+            return;
+        } 
+        
+        $this->saldo += $valorADepositar;  
     }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo){
+            echo"saldo indisponivel";
+            return;
+        } 
+        
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);  
+    }
+    
 }
