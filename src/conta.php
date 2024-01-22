12c0efc6669801363d8ce633ec4b43d73eddaf46
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2024-01-22 13:39:23 +0000

    Toda vez que criar uma conta , o saldo inicia com 0 e criação do metodo depositar.

--- src/conta.php
+++ src/conta.php
@@ -3,13 +3,21 @@
 class Conta {
     public string $cpfTitular;
     public string $nomeTitular;
-    public float $saldo;
+    public float $saldo = 0;
 
     public function sacar(float $valorASacar){
         if ($valorASacar > $this) {
             echo "Saldo indisponível";
         } else {
             $this->saldo -= $valorASacar;
+        }
+    }
+
+    public function depositar(float $valorADepositar): void{
+        if ($valorADepositar < 0){
+            echo "Valor precisa ser positivo";
+        } else {
+            $this->saldo += $valorADepositar;
         }
     }
 }
