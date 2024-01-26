a9097928226d8437c2fbde14a18d48ba72b4f248
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2024-01-26 11:25:08 +0000

    Criando a class abstract public function calculaBonificacao(): float;(Assim toda class que extends a funcionario, tem que ter essa class)

--- bonificacoes.php
+++ bonificacoes.php
@@ -5,31 +5,36 @@ require_once 'autoload.php';
 use Alura\Banco\Modelo\Cpf;
 use Alura\Banco\Modelo\Funcionario\Desenvolvedor;
 use Alura\Banco\Modelo\Funcionario\Diretor;
+use Alura\Banco\Modelo\Funcionario\EditorVideo;
 use Alura\Banco\Modelo\Funcionario\Gerente;
 use Alura\Banco\Service\ControladorDeBonificacoes;
 
 $umFuncionario = new Desenvolvedor(
         'Andre Aragão', 
         new Cpf('123.456.789-10'),
-        'Desenvolvedor',
         1000);
 
     $novoFuncionario = new Gerente(
         'Paloma Ribeiro', 
         new Cpf('987.654.321-10'),
-        'Desenvolvedora Front',
         3000
     );
 
     $diretor = new Diretor('Anderson Pereira',
     new Cpf('456.789.123-10'),
-    'Diretor',
     5000
+    );
+
+    $umEditor = new EditorVideo(
+        'Paulo',
+        new Cpf('456.789.123-10'),
+        1500
     );
 
     $controlador = new ControladorDeBonificacoes();
     $controlador->adicionaBonificaçãoDe($umFuncionario);
     $controlador->adicionaBonificaçãoDe($novoFuncionario);
     $controlador->adicionaBonificaçãoDe($diretor);
+    $controlador->adicionaBonificaçãoDe($umEditor);
 
     echo $controlador->recuperaTotal();
--- src/Modelo/Funcionario/Funcionario.php
+++ src/Modelo/Funcionario/Funcionario.php
@@ -7,13 +7,13 @@ use Alura\Banco\Modelo\Cpf;
 
 abstract class Funcionario extends Pessoa {
     
-    private string $cargo;
+ 
     private float $salario;
 
-    public function __construct(string $nome, Cpf $cpf, $cargo, $salario) {   
+    public function __construct(string $nome, Cpf $cpf, $salario) {   
         //Chamando o construtor da class Pessoa
         parent::__construct($nome, $cpf);
-        $this->cargo = $cargo;
+        
         $this->salario = $salario;
     }
 
@@ -22,10 +22,7 @@ abstract class Funcionario extends Pessoa {
         $this->nome = $nome;
     }
 
-    public function recuperaCargo() {
-        return $this->cargo;
-    }
-
+    
     public function recebeAumento(float $valorAumento): void {
         if ($valorAumento < 0 ) {
             echo "Aumento deve ser positivo";
@@ -39,9 +36,7 @@ abstract class Funcionario extends Pessoa {
         return $this->salario;
     }
 
-    public function calculaBonificacao(): float {
-        return $this->salario * 0.1;
-    }
+    abstract public function calculaBonificacao(): float;
 
 
 }
