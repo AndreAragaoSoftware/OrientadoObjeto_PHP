151b3ab8610477dc6fa6401ceb67d7bbe35f6efc
andrearagao.softwaredeveloper@gmail.com <64162795+AndreAragaoSoftware@users.noreply.github.com>
2024-01-30 12:48:25 +0000

    Validando o CPF com regexp

--- src/Modelo/Cpf.php
+++ src/Modelo/Cpf.php
@@ -8,6 +8,16 @@ final class Cpf
 
     public function __construct(string $numero)
     {
+        $numero = filter_var($numero, FILTER_VALIDATE_REGEXP,[
+            'options' => [
+                'regexp' => '/^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/'
+            ]
+        ]);
+        if ($numero === false) {
+            echo "CPF inválido";
+            exit();
+        }
+
         $this->numero = $numero;
     }
 
